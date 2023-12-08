ad947154aee21c0cbe9b8170991512d5e0882b4f
Pythagus <dmolina@etud.insa-toulouse.fr>
2023-12-08 17:50:59 +0100

    :art: Starting to setup the setup page

---
+++ appserver/static/javascript/view/setup/setup_view.js
@@ -0,0 +1,20 @@
+"use strict" ;
+
+define(["backbone", "jquery", "splunkjs/splunk"], function(Backbone, jquery, splunk_js_sdk) {
+    var View = Backbone.View.extend({
+        // -----------------------------------------------------------------
+        // Backbon Functions, These are specific to the Backbone library
+        // -----------------------------------------------------------------
+        initialize: function initialize() {
+            Backbone.View.prototype.initialize.apply(this, arguments) ;
+        },
+
+        render: function() {
+            this.container.innerHTML = "Let's start!" ;
+
+            return this ;
+        },
+    }) ;
+
+    return View ;
+}) ;
